10456bd5a6a97ac3a2f4582ea6d72c411fb8f5ee
Jean Paul <jmamanim@ulasalle.edu.pe>
2024-08-03 15:33:26 -0500

    Bug: Actualización de cliente arreglado

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -14,6 +14,7 @@ router.get("/clientes", getAllClientes);
 router.post("/cliente", createCliente);
 router.post("/cliente/find", findCliente);
 router.delete("/cliente/:id", deleteCliente);
+router.put("/cliente/:id", updateCliente);
 
 router.get("/cliente/:id", getCliente);
 
